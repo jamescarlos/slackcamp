d94f121965162ba47513cfc1c07c4d43c729180c
James Carlos <james@jamescarlos.com>
2014-03-13 14:23:22 -0700

    add default configuration file

---
+++ config.default.php
@@ -0,0 +1,14 @@
+<?php
+
+define('BASECAMP_USERNAME', 'basecamp_username');
+define('BASECAMP_PASSWORD', 'basecamp_password');
+define('BASECAMP_ID', '999999'); # http://basecamp.com/<this_id>
+define('SLACK_INSTANCE', 'slack_instance_name');
+define('SLACK_API_TOKEN', 'slack_api_token');
+define('SLACK_BOT_NAME', 'basecamp');
+define('SLACK_BOT_ICON', '');
+define('SLACK_DEFAULT_CHANNEL', '#basecamp');
+
+$slack_channels = array(
+    'Basecamp Project Name' => '#slack_channel_name'
+);
