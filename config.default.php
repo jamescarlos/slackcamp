1ff37d8dcc377235c070edb7b902143c706f08cc
James Carlos <james@jamescarlos.com>
2014-05-08 22:25:54 -0700

    added more info to aid setting up config file

--- config.default.php
+++ config.default.php
@@ -2,12 +2,12 @@
 
 define('BASECAMP_USERNAME', 'basecamp_username');
 define('BASECAMP_PASSWORD', 'basecamp_password');
-define('BASECAMP_ID', '999999'); # http://basecamp.com/<this_id>
-define('SLACK_INSTANCE', 'slack_instance_name');
+define('BASECAMP_ID', '999999'); // http://basecamp.com/<BASECAMP_ID>
+define('SLACK_INSTANCE', 'slack_instance_name'); // http://<SLACK_INSTANCE>.slack.com
 define('SLACK_API_TOKEN', 'slack_api_token');
 define('SLACK_BOT_NAME', 'basecamp');
-define('SLACK_BOT_ICON', '');
-define('SLACK_DEFAULT_CHANNEL', '#basecamp');
+define('SLACK_BOT_ICON', ''); // full URL to bot icon
+define('SLACK_DEFAULT_CHANNEL', '#basecamp'); // the default channel that messages will be posted to
 
 $slack_channels = array(
     'Basecamp Project Name' => '#slack_channel_name'
